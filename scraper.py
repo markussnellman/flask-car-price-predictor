695c5f8990bfd1e74f1a128eed0ce761f19f7939
Markus Snellman <ma6662sn@fys.local>
2024-01-23 09:28:58 +0100

    Removed redundant main.py

--- scraper.py
+++ scraper.py
@@ -58,6 +58,42 @@ def scrape(manufacturer: str, model: str) -> list[dict]:
 
 
 if __name__ == "__main__":
-    manufacturer = 'hyundai'
-    model = 'tucson'
-    print(scrape(manufacturer, model))
+    import os
+    from utils import save_to_cloud
+
+    url = os.environ.get('RENDER_TEST_DB_EXTERNAL_URL')
+
+    cars = {'Audi': ['A6', 'A4', 'A3', 'A5', 'Q5'],
+            'BMW': ['520', '320', 'X3', 'X5', '118', 'X1'],
+            'Citroen': ['C3', 'C4', 'C5'],
+            'Dacia': ['Sandero', 'Duster'],
+            'Fiat': ['500'],
+            'Honda': ['CR-V', 'Jazz'],
+            'Hyundai': ['i10', 'i30', 'Bayon'],
+            'Mazda': ['CX-60', '2', 'CX-30', 'MX-30'],
+            'Mercedes': ['E300'],
+            'Mitsubishi': ['Outlander'],
+            'Volvo': ['V60', 'V70', 'XC90', 'V40', 'V90', 'S60'],
+            'Volkswagen': ['Tiguan', 'Touran', 'ID-4', 'T-Roc'],
+            'Kia': ['Niro', 'Sorento'],
+            'Toyota': ['RAV4', 'C-HR', 'Corolla-Verso', 'Auris', 'Yaris', 'Proace'],
+            'Nissan': ['Qashqai', 'Juke'],
+            'Opel': ['Vivaro', 'Corsa'],
+            'Peugeot': ['3008', '308', '2008'],
+            'Renault': ['Clio', 'Captur', 'Kangoo'],
+            'Seat': ['Leon', 'Ibiza', 'Arona'],
+            'Ford': ['Kuga', 'Focus', 'Fiesta', 'Mondeo', 'Mustang'],
+            'Skoda': ['Octavia', 'Superb'],
+            'Subaru': ['Outback']}
+
+    """
+    VW ID-4 probably not inserted to DB. Error: List index out or range (How's that possible?)
+    Skoda enyaq same problem
+    Wonder if it's something with the electric cars
+    """
+    manufacturers = ['Skoda', 'Subaru']
+
+    for manufacturer in manufacturers:
+        for model in cars[manufacturer]:
+            result = scrape(manufacturer, model)
+            save_to_cloud(result, 'cars', url)
